386daa0ea18c1587db58be3fa241503e52b5e536
Tejas Waghmare <tejaswaghmare.work@gmail.com>
2026-08-26 14:02:07 +0530

    Initial project setup

---
+++ src/main/java/com/nexoraa/resumeextractor/ResumeInformationExtractorApplication.java
@@ -0,0 +1,13 @@
+package com.nexoraa.resumeextractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeInformationExtractorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeInformationExtractorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexoraa/resumeextractor/ResumeInformationExtractorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexoraa.resumeextractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeInformationExtractorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
