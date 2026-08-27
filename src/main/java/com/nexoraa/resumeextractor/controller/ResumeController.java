82c43d573f7cb365bc2f0c60a8826d5d06972138
Tejas Waghmare <tejaswaghmare.work@gmail.com>
2026-08-27 21:36:21 +0530

    refactor: separate AI prompt and improve controller response

--- src/main/java/com/nexoraa/resumeextractor/controller/ResumeController.java
+++ src/main/java/com/nexoraa/resumeextractor/controller/ResumeController.java
@@ -2,6 +2,7 @@ package com.nexoraa.resumeextractor.controller;
 
 import com.nexoraa.resumeextractor.model.CandidateProfile;
 import com.nexoraa.resumeextractor.service.ResumeService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,8 +22,13 @@ public class ResumeController {
 	}
 	
 	@PostMapping("/upload")
-	public CandidateProfile uploadResume(@RequestParam("file") MultipartFile file) throws IOException {
-		return resumeService.uploadResume(file);
+	public ResponseEntity<CandidateProfile> uploadResume(
+			@RequestParam("file") MultipartFile file
+	) throws IOException {
+		
+		CandidateProfile candidateProfile =
+				resumeService.uploadResume(file);
+		
+		return ResponseEntity.ok(candidateProfile);
 	}
-
 }
